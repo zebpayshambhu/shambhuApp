f7d950222c8590349ad4d8c3a11b23116576b815
Shambhu <shambhu.pasi@zebpay.com>
2018-02-02 13:23:11 +0530

    new changes

--- EntityLayer/Model/AccountModel.cs
+++ EntityLayer/Model/AccountModel.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EntityLayer.Model
 {
-
-
-    class AccountModel
+    [MetadataType(typeof(AccountModel))]
+    public partial class tblAccount
+    {
+    }
+    public class AccountModel
     {
     }
 
